3cb53f1eaa7a76473ceafcaad51997cbe849872b
TinotendaMuponda <44811208+TinotendaMuponda@users.noreply.github.com>
2023-08-07 19:11:09 +0200

    Create WeatherController.java

---
+++ src/main/java/com/tinoprojects/weatherforecastapp/controller/WeatherController.java
@@ -0,0 +1,24 @@
+package com.tinoprojects.weatherforecastapp.controller;
+
+import com.tinoprojects.weatherforecastapp.domain.WeatherData;
+import com.tinoprojects.weatherforecastapp.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/weather-forecast")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @GetMapping(params = {"city"})
+    public ResponseEntity<WeatherData> getWeatherInfo(@RequestParam String city){
+        return ResponseEntity.ok(weatherService.getWeatherInfo(city));
+    }
+
+}
