44aa580bfd828393c0a52ee066b5f45ab2dd5579
TinotendaMuponda <44811208+TinotendaMuponda@users.noreply.github.com>
2023-08-07 15:07:34 +0200

    Add users controller initial CRUD methods

--- src/main/java/com/tinoprojects/weatherforecastapp/controller/UserController.java
+++ src/main/java/com/tinoprojects/weatherforecastapp/controller/UserController.java
@@ -1,10 +1,40 @@
 package com.tinoprojects.weatherforecastapp.controller;
 
+import com.tinoprojects.weatherforecastapp.domain.User;
+import com.tinoprojects.weatherforecastapp.service.UsersService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UsersService usersService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(User user){
+        return new ResponseEntity<>(usersService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(){
+        return  ResponseEntity.ok(usersService.getUsers());
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<User> getUserById(Long id){
+        return  ResponseEntity.ok(usersService.getUserById(id));
+    }
+
+    @GetMapping(params = {"username"})
+    public ResponseEntity<User> getUserByUsername(@RequestParam String username){
+        return  ResponseEntity.ok(usersService.getUserByUsername(username));
+    }
+
+
 }
