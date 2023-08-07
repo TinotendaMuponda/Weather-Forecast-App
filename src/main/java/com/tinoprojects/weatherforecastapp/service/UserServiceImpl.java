5bcbd7a99fe559f7567ca11208d4e726d24cb396
TinotendaMuponda <44811208+TinotendaMuponda@users.noreply.github.com>
2023-08-07 14:42:41 +0200

    Implement user service methods

--- src/main/java/com/tinoprojects/weatherforecastapp/service/UserServiceImpl.java
+++ src/main/java/com/tinoprojects/weatherforecastapp/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.tinoprojects.weatherforecastapp.service;
 
 import com.tinoprojects.weatherforecastapp.UserRepository;
 import com.tinoprojects.weatherforecastapp.domain.User;
+import com.tinoprojects.weatherforecastapp.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,21 +15,21 @@ public class UserServiceImpl implements UsersService {
     private final UserRepository userRepository;
     @Override
     public User createUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public User getUserByUsername(String username) {
-        return null;
+        return userRepository.findByUsername(username).orElseThrow(()->new ResourceNotFoundException("User with username not Found"));
     }
 
     @Override
     public User getUserById(Long id) {
-        return null;
+        return userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Username with ID not Found"));
     }
 }
