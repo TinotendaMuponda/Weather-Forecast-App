8fca4d2a3a5801c58d6b05b37087988cbe556d2b
TinotendaMuponda <44811208+TinotendaMuponda@users.noreply.github.com>
2023-08-07 13:31:09 +0200

    Create User.java

---
+++ src/main/java/com/tinoprojects/weatherforecastapp/domain/User.java
@@ -0,0 +1,25 @@
+package com.tinoprojects.weatherforecastapp.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String city;
+
+
+}
