829280c90442b874c530d902cc33ddd4e5bc03e9
TinotendaMuponda <44811208+TinotendaMuponda@users.noreply.github.com>
2023-08-07 13:50:14 +0200

    Inject User Repository for UsersService class

--- src/main/java/com/tinoprojects/weatherforecastapp/service/UserServiceImpl.java
+++ src/main/java/com/tinoprojects/weatherforecastapp/service/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.tinoprojects.weatherforecastapp.service;
 
+import com.tinoprojects.weatherforecastapp.UserRepository;
 import com.tinoprojects.weatherforecastapp.domain.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UsersService {
+
+    private final UserRepository userRepository;
     @Override
     public User createUser(User user) {
         return null;
