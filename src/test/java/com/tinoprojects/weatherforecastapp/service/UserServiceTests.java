bd7153443988451605d6b4cde0288d630184ff75
TinotendaMuponda <44811208+TinotendaMuponda@users.noreply.github.com>
2023-08-07 14:11:38 +0200

    Creating failing unit tests for UsersService class
    The junit tests includes testCreateUser, testGetUsers, testGetUserById, testGetUserByUsername and checking exception not found exception for a specific user

--- src/test/java/com/tinoprojects/weatherforecastapp/service/UserServiceTests.java
+++ src/test/java/com/tinoprojects/weatherforecastapp/service/UserServiceTests.java
@@ -1,4 +1,123 @@
 package com.tinoprojects.weatherforecastapp.service;
 
+import com.tinoprojects.weatherforecastapp.UserRepository;
+import com.tinoprojects.weatherforecastapp.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RunWith(value = MockitoJUnitRunner.class)
 public class UserServiceTests {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    public void testCreateUser(){
+        User user  = new User();
+        user.setId(1L);
+        user.setUsername("tmuponda");
+        user.setFirstName("Tinotenda");
+        user.setLastName("Muponda");
+        user.setCity("Harare");
+        user.setCountry("Zimbabwe");
+//        Mock Behaviour
+
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+
+        User actualUser = userService.createUser(user);
+
+        assertNotNull(actualUser);
+        assertEquals(user, actualUser);
+    }
+
+    @Test
+    public void testGetUserByUsername(){
+        String username = "tmuponda";
+
+        User user  = new User();
+        user.setId(1L);
+        user.setUsername("tmuponda");
+        user.setFirstName("Tinotenda");
+        user.setLastName("Muponda");
+        user.setCity("Harare");
+        user.setCountry("Zimbabwe");
+//        Mock Behaviour
+
+        Mockito.when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
+
+        User actualUser = userService.getUserByUsername(username);
+
+        assertNotNull(actualUser);
+        assertEquals(user, actualUser);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetUserByUsername_ThrowsNotFoundExceptionIfUserNotFound(){
+        String username = "tmuponda";
+
+        User user  = new User();
+        user.setId(1L);
+        user.setUsername("tmuponda");
+        user.setFirstName("Tinotenda");
+        user.setLastName("Muponda");
+        user.setCity("Harare");
+        user.setCountry("Zimbabwe");
+//        Mock Behaviour
+
+        Mockito.when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+    }
+    @Test(expected = RuntimeException.class)
+    public void testGetUserById_ThrowsNotFoundExceptionIfUserNotFound(){
+        Long id = 1L;
+
+        User user  = new User();
+        user.setId(1L);
+        user.setUsername("tmuponda");
+        user.setFirstName("Tinotenda");
+        user.setLastName("Muponda");
+        user.setCity("Harare");
+        user.setCountry("Zimbabwe");
+//        Mock Behaviour
+
+        Mockito.when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+    }
+
+    @Test
+    public void testGetUsers(){
+        Long id = 1L;
+
+        User user  = new User();
+        user.setId(1L);
+        user.setUsername("tmuponda");
+        user.setFirstName("Tinotenda");
+        user.setLastName("Muponda");
+        user.setCity("Harare");
+        user.setCountry("Zimbabwe");
+//        Mock Behaviour
+
+        Mockito.when(userRepository.findAll()).thenReturn(List.of(user));
+
+        List<User> actualUsers = userService.getUsers();
+
+        assertNotNull(actualUsers);
+        assertEquals(user, actualUsers);
+        assertEquals(1, actualUsers);
+    }
+
+
 }
